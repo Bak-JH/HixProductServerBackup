063a4299f977877b26ab886bf5da415e0244dd26
legokangpalla <legokangpalla@gmail.com>
2020-07-12 19:27:25 +0900

    pretty-fying django admin page

--- product/admin.py
+++ product/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from product.models import Product, ProductSerial
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(ProductSerial)
 
+@admin.register(ProductSerial)
+class ProductSerialInstanceAdmin(admin.ModelAdmin):
+    list_display = ('serial_number', 'owner', 'product', 'created_date', 'expire_date')
+    list_filter = ('serial_number', 'owner', 'product', 'created_date', 'expire_date')
+    def product_name(self, product_serial):
+        return product_serial.product.name
+
+
+@admin.register(Product)
+class ProductInstanceAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
