d9d35670609904a52159158245c9e416130b1e8b
Bak-JH <bakjh.6280@gmail.com>
2020-07-18 14:20:35 +0900

    remove .zip

--- setup/views.py
+++ setup/views.py
@@ -1,7 +1,6 @@
 import fnmatch
 import json
 import os
-import zipfile
 
 # Create your views here.
 from django.conf import settings
@@ -9,22 +8,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def load_zipfile():
-    target_dir = os.path.join(settings.BASE_DIR, "setup/")#TODO: edit path
-    for filename in os.listdir(target_dir):
-        if fnmatch.fnmatch(filename, "*DentSlicer*.zip"):
-            with zipfile.ZipFile(os.path.abspath(target_dir + filename), 'r') as unzip:
-                for name in unzip.namelist():
-                    if fnmatch.fnmatch(name, "*.json"):
-                        with unzip.open(name) as infoFile:
-                            settings.LATEST_INFO = json.load(infoFile)
-                    if fnmatch.fnmatch(name, "*.exe"):
-                        with unzip.open(name) as exe:
-                            settings.setupFile = os.path.abspath(unzip.extract(name, 'setup'))
+def load_updateFile(target_dir):
+    for (path, dir, files) in os.walk(target_dir):
+        for filename in files:
+            if fnmatch.fnmatch(filename, "*.json"):
+                with open(os.path.join(path, filename)) as infoFile:
+                    settings.LATEST_INFO = json.load(infoFile)
+            if fnmatch.fnmatch(filename, "*.exe"):
+                with open(os.path.join(path, filename)) as exe:
+                    settings.setupFile = os.path.abspath(exe.name)
 
 
 def download_slicer(request):
-    load_zipfile()
+    load_updateFile(os.path.join(settings.BASE_DIR, "setup/"))#TODO: edit path
     with open(settings.setupFile, 'r+b') as fp:
         data = fp.read()
     response = HttpResponse(content_type='application/force-download')
@@ -35,7 +31,7 @@ def download_slicer(request):
 
 
 def get_latest(request):
-    load_zipfile()
+    load_updateFile(os.path.join(settings.BASE_DIR, "setup/"))#TODO: edit path
     file = render(request, os.path.join(settings.BASE_DIR, "setup/templates/appcast_template.html"),
                   {'release': settings.LATEST_INFO})
     return HttpResponse(file, content_type="application/xml")
