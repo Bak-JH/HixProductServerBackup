119c15818d86a24c0bbff2b1d9f0c652452fe925
Bak-JH <bakjh.6280@gmail.com>
2020-07-24 04:41:13 +0900

    edit setup

--- setup/views.py
+++ setup/views.py
@@ -1,47 +1,31 @@
 import fnmatch
 import json
 import os
+import mimetypes
 
 # Create your views here.
 from django.conf import settings
 from django.http import HttpResponse
-from django.shortcuts import render
 
+def get_update_manifest(request, product_name):
+    target_path = os.path.join(settings.UPDATE_FILE_DIR, product_name)
+    data = sorted(os.listdir(target_path))
+    return HttpResponse(json.dumps(data))
 
-def load_updateFile(target_dir):
-    for (path, dir, files) in os.walk(target_dir):
-        for filename in files:
-            if fnmatch.fnmatch(filename, "*.json"):
-                with open(os.path.join(path, filename)) as infoFile:
-                    settings.LATEST_INFO = json.load(infoFile)
-            if fnmatch.fnmatch(filename, "*.exe"):
-                with open(os.path.join(path, filename)) as exe:
-                    settings.setupFile = os.path.abspath(exe.name)
+def get_file(request, product_name, file_name):
+    target_path = os.path.join(settings.UPDATE_FILE_DIR, product_name)
 
-
-def download_slicer(request):
-    load_updateFile(os.path.join(settings.BASE_DIR, "setup/"))#TODO: edit path
-    with open(settings.setupFile, 'r+b') as fp:
-        data = fp.read()
     response = HttpResponse(content_type='application/force-download')
-    response['Content-Disposition'] = "attachment; filename=DentSlicerSetup.exe"
-    response.write(data)
 
+    try:
+        with open(os.path.join(target_path + "\\" + file_name)) as file:
+            data = file.read()
+    except:
+        with open(os.path.join(target_path + "\\" + file_name), 'r+b') as bin_file:
+            data = bin_file.read()
+
+    response.write(data)
     return response
 
 
-def get_latest(request):
-    load_updateFile(os.path.join(settings.BASE_DIR, "setup/"))#TODO: edit path
-    file = render(request, os.path.join(settings.BASE_DIR, "setup/templates/appcast_template.html"),
-                  {'release': settings.LATEST_INFO})
-    return HttpResponse(file, content_type="application/xml")
 
-
-def download_firmware(request):
-    with open(os.path.join(settings.BASE_DIR, 'setup/DentSlicer-1.1.2.zip'), 'r+b') as fp: #TODO: edit path
-        data = fp.read()
-    response = HttpResponse(content_type='application/force-download')
-    response['Content-Disposition'] = "attachment; filename=FirmwareSetup.zip"
-    response.write(data)
-
-    return response
