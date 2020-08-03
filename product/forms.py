07d0bf605d872a8400faf26675348784575034b3
Bak-JH <bakjh.6280@gmail.com>
2020-08-03 18:24:01 +0900

    add login, signup view

--- product/forms.py
+++ product/forms.py
@@ -3,9 +3,10 @@ import datetime
 from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
 from product.models import Product, ProductSerial
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Row, Column
+from crispy_forms.layout import Layout, Row, Column, Submit, Field
 
 
 class RegisterSerialForm(forms.Form):
@@ -30,17 +31,69 @@ class RegisterSerialForm(forms.Form):
         return data
 
 
-class LoginForm(forms.Form):
-    id = forms.CharField(label="ID")
-    password = forms.CharField(widget=forms.PasswordInput, label=_("Password"))
+class LoginForm(forms.ModelForm):
+    username = forms.CharField(label='ID')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
+        self.helper = FormHelper(self)
         self.helper.layout = Layout(
             Row(
-                Column('id', css_class='form-group col-md-6 mb-0'),
-                Column('password', css_class='form-group col-md-6 mb-0'),
+                Column(
+                    Field('username', css_class='form-control mb-4'),
+                    css_class='col-sm-6'
+                ),
+                Column(
+                    Field('password', css_class='form-control mb-4'),
+                    css_class='col-sm-6'
+                ),
+                css_class='form-row'
+            ),
+            Row(
+                Submit('submit', 'Sign in', css_class='btn btn-primary w-100 m-1'),
                 css_class='form-row'
             )
         )
+    class Meta:
+        model = User
+        widgets = {'password':forms.PasswordInput}
+        fields = ['username','password']
+
+class SignupForm(forms.ModelForm):
+    username = forms.CharField(label='ID')
+    email = forms.EmailField(label='Email', required=False)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.layout = Layout(
+            Row(
+                 Column(
+                    Field('email', css_class='form-control mb-4'),
+                    css_class='col-sm-12'
+                ),
+                css_class='form-row'
+            ),
+            Row(
+                Column(
+                    Field('username', css_class='form-control mb-4'),
+                    css_class='col-sm-6'
+                ),
+               
+                Column(
+                    Field('password', css_class='form-control mb-4'),
+                    css_class='col-sm-6'
+                ),
+                css_class='form-row'
+            ),
+            Row(
+                Submit('submit', 'Sign up', css_class='btn btn-primary w-100 m-1'),
+                css_class='form-row'
+            )
+        )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
--- product/views.py
+++ product/views.py
@@ -7,12 +7,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseForbidden, HttpResponseNotFound, HttpResponse, HttpResponseRedirect
 from django.core.exceptions import MultipleObjectsReturned 
 from django.urls import reverse
 from product.models import Product, ProductSerial
-from product.forms import RegisterSerialForm, LoginForm
+from product.forms import RegisterSerialForm, LoginForm, SignupForm
 from django.db import models
 
 from allauth.account.signals import user_signed_up
@@ -79,13 +81,54 @@ def register(req):
     
     return HttpResponseRedirect(reverse('registration_done'))
 
-@require_GET
-def product_login(req):
-    """Checks logged in status"""
-    if req.user.is_authenticated:
+def product_signup(request):
+    if request.user.is_authenticated:
         return HttpResponseRedirect('/product/login_redirect/')
-    form = LoginForm()
-    return render(req, 'product/login.html', {'form': form})
+
+    else:
+        if request.method == "POST":
+            form = SignupForm(request.POST)
+            if form.is_valid():
+                new_user = User.objects.create_user(**form.cleaned_data)
+                return HttpResponseRedirect(reverse('product_login'))
+
+        #get
+        else:
+            form = SignupForm()
+    return render(request, 'product/signup.html', {'form': form})
+
+def product_login(request):
+    """Checks logged in status"""
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/product/login_redirect/')
+    else:
+        if request.method == 'POST':
+            print(request.POST)
+            form = LoginForm(request.POST)
+            id = request.POST['username']
+            pw = request.POST['password']
+            user = authenticate(username=id, password=pw)
+            if user is not None:
+                login(request, user=user)
+                return HttpResponseRedirect(reverse('product_login_redirect'))
+            else:
+                try: 
+                    user = User.objects.get(username=request.POST['username'])
+                    error = 'Password is incorrect'
+                except:
+                    error = 'We cannot find an account with that ID'
+
+                context = {
+                    'form': form,
+                    'error': error
+                }
+                return render(request, 'product/login.html', context)
+
+        #get
+        else:
+            form = LoginForm()
+        
+        return render(request, 'product/login.html', {'form': form})
 
 
 @login_required
