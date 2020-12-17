b375431460cade9077533c82bc54f5fc91453450
Bak-JH <bakjh.6280@gmail.com>
2020-12-17 10:53:36 +0900

    remove useless jwt part

--- management/urls.py
+++ management/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     #url('add-resin/', views.add_resinInfo),
     path('add-serial/', views.add_serial),
-    path('current_user/', views.current_user),
-    path('users/', views.UserList.as_view()),
 ]
--- management/views.py
+++ management/views.py
@@ -10,7 +10,6 @@ from datetime import date
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import UserSerializer, UserSerializerWithToken
 from rest_framework.views import APIView
 from rest_framework import permissions, status
 
@@ -42,30 +41,6 @@ def add_serial(request):
     else:
         form = AddProductSerialForm()
     return render(request, 'management/add_serial.html', {'form': form})
-
-@api_view(['GET'])
-def current_user(request):
-    """
-    Determine the current user by their token, and return their data
-    """
-    
-    serializer = UserSerializerWithToken(request.user)
-    return Response(serializer.data)
-
-class UserList(APIView):
-    """
-    Create a new user. It's called 'UserList' because normally we'd have a get
-    method here too, for retrieving a list of all User objects.
-    """
-
-    permission_classes = (permissions.AllowAny,)
-
-    def post(self, request, format=None):
-        serializer = UserSerializerWithToken(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # @login_required
 # @permission_required('management.edit_resin', login_url="services.hix.co.kr/accounts/login", raise_exception=True)
