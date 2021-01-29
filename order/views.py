a1de34f80c6cf4f6cd5a7ace2f8f577896c4bfb3
BakJH <bakjh.6280@gmail.com>
2021-01-29 15:48:41 +0900

    wip

--- order/views.py
+++ order/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from lib.BootpayApi import BootpayApi
 import json 
 from django.contrib.auth.models import User
@@ -8,20 +8,22 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url="/product/login")
 def subscribe(request):
-    order_id = request.GET.get('id')
-    if order_id:
+    billing_id = request.GET.get('id')
+    if billing_id:
+        print(billing_id)
         bootpay = BootpayApi('59a4d32b396fa607c2e75e00', 't3UENPWvsUort5WG0BFVk2+yBzmlt3UDvhDH2Uwp0oA=')
         access_token = bootpay.get_access_token()
-        result = bootpay.subscribe_billing(order_id, 'test', 3000, '12345', [], {'username': 'test'})
+        result = bootpay.subscribe_billing(billing_id, '테스트', 1000, 'id_dent1', [], {'username': 'test'})
         receipt = result['data']['receipt_url']
         return HttpResponseRedirect(receipt)
     else:
         return render(request, 'subscribe.html')
 
 @login_required(login_url="/product/login")
-def cancel_subscribe(request):
+def cancel_subscribe(request, billing_id):
+    print(billing_id)
     bootpay = BootpayApi('59a4d32b396fa607c2e75e00', 't3UENPWvsUort5WG0BFVk2+yBzmlt3UDvhDH2Uwp0oA=')
     response = bootpay.get_access_token()
     if response['status'] is 200:
-        print(User.objects.get())
-        bootpay.destroy_subscribe_billing_key('[[ billing key ]]')
+        print(bootpay.destroy_subscribe_billing_key(billing_id))
+        return HttpResponse(bootpay.destroy_subscribe_billing_key(billing_id))
